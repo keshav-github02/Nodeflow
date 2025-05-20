81e4d00ac65c9c748c3929aadac7f87f151e0786
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-05-20 22:42:45 +0530

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const dotenv = require('dotenv').config();
 
 const app = express();
 const port= process.env.PORT || 5000;
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
     }
