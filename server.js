2921c9d71f830e4d2e2ec5b6fe38299a76d58a61
keshav-github02 <keshav.2022ug1022@iiitranchi.ac.in>
2025-05-21 16:59:37 +0530

    making routes

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();  
 
-
 const app = express();
-const port= process.env.PORT || 5000;
-
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 
 
-
+app.use("/api/contacts", require("./routes/contactRoutes"));
+// Middleware to parse JSON requests
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-    }
-);
+});
